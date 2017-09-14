634000fa4227a916bf4762e7791eb26da02e4dac
FrankEstevez <frankestevez46@gmail.com>
2017-09-14 16:51:10 -0400

    Fixed spelling error

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let name = prompt ('Name a person')
 let place = prompt ('Name a place')
-let adjective = ["Long", 'young', 'bad']
+let adjective = ['long', 'young', 'bad']
 let plural = ['babies', 'buses', 'dwarves']
+
 
 document.write('Last summer, my mom and dad took me and' 
 	+ ' ' + name + ' ' + 'on a trip to' + ' ' + place 
 	+ '.' + 'The weather there is very' 
-	+ ' ' + adjective[Math.floor(adjective.lenght*Math.random())]  + '!' 
+	+ ' ' + adjective [Math.floor(adjective.length*Math.random())] + '!' 
 	+ ' ' + 'Northeren' + ' ' + place + ' ' + 'has many' 
-	+ ' ' + plural[Math.floor(plural.lenght*Math.random())] + ', and they make' 
-	+ ' ' + adjective[Math.floor(adjective.lenght*Math.random())] 
-	+ ' ' + plural[Math.floor(plural.lenght*Math.random())] + ' there.'
-	)
+	+ ' ' + plural[Math.floor(plural.length*Math.random())] + ', and they make' 
+	+ ' ' + adjective[Math.floor(adjective.length*Math.random())] 
+	+ ' ' + plural[Math.floor(plural.length*Math.random())] + ' there.'
+	);
 
 
 
